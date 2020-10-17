620e227f67dc83ca686c937e77322f223bd4ba7b
Grigorovich Tatyana <gtm003@gmail.com>
2020-10-17 15:07:34 +0300

    feat: add function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let count = 0;
+  let resalt = true;
+  while (resalt && str !== '') {
+    bracketsConfig.forEach(function(item, i, arr) {
+    if (str.indexOf(item[0] + item[1]) >= 0) count++;
+    //console.log (count);
+    return count;
+    });
+    if (count > 0) {
+      bracketsConfig.forEach(function(item, i, arr) {
+      //str = str.replace(array[0] + array[1], '');
+      str = str.replace(item[0] + item[1], '');
+      //console.log( str);
+      });
+      count=0;
+    } else resalt = false;
+  }
+  return resalt;
 }
